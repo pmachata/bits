4c7646080e1869bf35f1493c6488e27691bae53a
Petr Machata <pmachata@gmail.com>
2012-05-27 21:17:57 +0200

    Add a prime number iterator

--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ hash: hash.cc hash.hh tests.hh
 slist: slist.cc slist.hh forward_vec.hh tests.hh
 assoc_vec: assoc_vec.cc assoc_vec.hh tests.hh
 rbtree: rbtree.cc rbtree.hh tests.hh
+prime_iterator: prime_iterator.cc prime_iterator.hh
 times: times.cc $(wildcard *.hh)
-times hash slist assoc_vec rbtree: CXXFLAGS = -std=c++0x -Wall -g -O2
+prime_iterator times hash slist assoc_vec rbtree: CXXFLAGS = -std=c++0x -Wall -g -O2
---
+++ prime_iterator.hh
@@ -0,0 +1,140 @@
+/*
+ * Implementation of prime number iterator based on Sieve of
+ * Eratosthenes.
+ *
+ * Copyright (C) 2012 Petr Machata
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PRIME_ITERATOR_H_
+#define _PRIME_ITERATOR_H_
+
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+template <class T = unsigned>
+class prime_iterator
+{
+  typedef std::vector<bool> sieve_t;
+  sieve_t sieve;
+  T cur;
+
+  void
+  extend_sieve (sieve_t::size_type end)
+  {
+    if (sieve.size () >= end)
+      return;
+
+    sieve_t::size_type base = sieve.size ();
+    sieve.resize (end, true);
+
+    sieve_t::size_type last = (sieve_t::size_type)std::sqrt (end);
+    if (last * last < end)
+      ++last;
+
+    /* This sub-sieve is necessary so that we know which factors were
+       processed in this round.  What's in SIEVE just tells us what
+       was processed in the past, but for the new part of sieve, we
+       need to do the computation again.  */
+    std::vector<bool> local (last, true);
+
+    for (T i = 2; i < last; ++i)
+      if (local[i])
+	{
+	  for (T j = i; j < last; j += i)
+	    local[j] = false;
+	  for (T j = ((base + i - 1)/ i) * i; j < end; j += i)
+	    sieve[j] = false;
+	}
+  }
+
+  bool
+  is_prime (T val)
+  {
+    extend_sieve (val + 1);
+    return sieve[val];
+  }
+
+public:
+  prime_iterator (T initial = 2)
+    : cur (initial)
+  {
+    extend_sieve (2 * initial);
+    if (!is_prime (initial))
+      ++*this;
+  }
+
+  prime_iterator &
+  operator = (T rhs)
+  {
+    cur = rhs;
+    extend_sieve (2 * cur);
+    if (!is_prime (cur))
+      ++*this;
+    return *this;
+  }
+
+  T
+  operator * () const
+  {
+    return cur;
+  }
+
+  prime_iterator &
+  operator ++ ()
+  {
+    do
+      if (cur >= sieve.size () - 1)
+	extend_sieve (sieve.size () * 2);
+    while (!sieve[++cur]);
+    return *this;
+  }
+
+  prime_iterator
+  operator ++ (int)
+  {
+    prime_iterator copy = *this;
+    ++*this;
+    return copy;
+  }
+
+  bool
+  operator < (T rhs) const
+  {
+    return cur < rhs;
+  }
+
+  bool
+  operator == (T rhs) const
+  {
+    return cur == rhs;
+  }
+
+  bool
+  operator == (prime_iterator rhs) const
+  {
+    return *this == rhs.cur;
+  }
+
+  template <class U>
+  bool
+  operator != (U rhs) const
+  {
+    return !(*this == rhs);
+  }
+};
+
+#endif /* _PRIME_ITERATOR_H_ */
